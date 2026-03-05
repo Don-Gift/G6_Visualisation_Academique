2fc1ba8877e7cffaec61b4c9c55eeaa0cae9963e
Don-Gift <niyingabiyedondivin@gmail.com>
2026-03-05 17:32:00 +0200

    Initial commit

---
+++ app/src/main/java/com/itn/visualisation/models/Cours.java
@@ -0,0 +1,23 @@
+package com.itn.visualisation.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Cours {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("nom_cours")
+    private String nomCours;
+
+    @SerializedName("code_cours")
+    private String codeCours;
+
+    @SerializedName("description")
+    private String description;
+
+    public int getId() { return id; }
+    public String getNomCours() { return nomCours; }
+    public String getCodeCours() { return codeCours; }
+    public String getDescription() { return description; }
+}
